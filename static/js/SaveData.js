93640a3131db24e717c5e2fd4684b6165a41e884
elobot9 <elizabeth.long321@gmail.com>
2016-07-19 04:12:14 -0400

    fixed nback score bug, created generalized data saving functions

---
+++ static/js/SaveData.js
@@ -0,0 +1,24 @@
+var saveSingleTaskData = function(){
+	var exp1_data = new Array();
+	for (var i = 0; i < window.words_collection.length; i++){
+		var singletask_collection = window.words_collection.get(i);
+		exp1_data.push(singletask_collection.get('answer'))
+	}
+	console.log(exp1_data)
+	psiTurk.recordTrialData(exp1_data);
+	psiTurk.saveData()
+}
+
+var saveDualTaskData = function(){
+	var exp2_data = new Array();
+	for (var i = 0; i < window.dual_task_trials.length; i++){
+		var dualtask_collection = window.dual_task_trials.get(i);
+		var nback_accuracy = dualtask_collection.percentCorrect();
+		var words_response = dualtask_collection.get('answer');
+		exp2_data.push("nback accuracy = " + nback_accuracy + " remembered words = " + words_response);
+	}
+	console.log(exp2_data);
+	psiTurk.recordTrialData(exp2_data);
+	psiTurk.saveData()
+
+}
